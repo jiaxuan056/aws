37e607843a4cb37e02ca9bc63db0385b2b86cdd0
jiaxuan056 <163286298+jiaxuan056@users.noreply.github.com>
2025-04-29 17:17:04 +0800

    Update helper.php

--- PhpProject1/helper.php
+++ PhpProject1/helper.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'jiaxuan');
-define('DB_PASS', 'jiaxuan');
+define('DB_PASS', 'lab-password');
 define('DB_NAME', 'cloud');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
